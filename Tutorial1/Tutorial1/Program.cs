5202b8902584f13b65b3ef60dc91f2820a442465
RazzDoes <conradgeisel@gmail.com>
2018-07-24 21:21:32 -0400

    It Begins...
    ...with crappy code.

--- Tutorial1/Tutorial1/Program.cs
+++ Tutorial1/Tutorial1/Program.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 
-namespace Tutorial1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-			Console.ReadLine();
+namespace Tutorial1 {
+    class Program {
+        static void Main(string[] args){
+			// 5
+
+			int heroHitPoints, monsterHitPoints, heroMagicPoints;
+			string myName, monsterName;
+			bool isAlive, isActive;
+
+			heroHitPoints = 30;
+			myName = "Something";
+			isAlive = true;
+            
+			monsterHitPoints = 10;
+			monsterName = "Mage";
+			isActive = false;
+			heroHitPoints = 15;
         }
     }
 }
