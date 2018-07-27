884318d046bb6a9e688ec5775bd045002b82e9fc
RazzDoes <conradgeisel@gmail.com>
2018-07-26 20:18:44 -0400

    Basic battle loop / setting up AI

--- Tutorial1/Tutorial1/Battle.cs
+++ Tutorial1/Tutorial1/Battle.cs
@@ -3,6 +3,10 @@ namespace RPGTutorial
 {
     public class Battle
     {
+		string userChoice;
+		int damage;
+		Random rand;
+
         public Battle(Hero hero, Monster monster){
 			Console.WriteLine("{0} is facing a {1}.", hero.identifier, monster.identifier);
 			BattleLoop(hero, monster);
@@ -11,7 +15,12 @@ namespace RPGTutorial
 		public void BattleLoop(Hero hero, Monster monster){
 			do{
 				PrintStatus(hero, monster);
+				userChoice = PrintChoice();
+				Console.WriteLine();
+				ProcessChoice(userChoice, hero, monster);
+				monster.isAlive = CheckHealth(monster.currentHealth);
 				Console.ReadLine();
+				Console.Clear();
 			} while (hero.isAlive == true && monster.isAlive == true);
 		}
 
@@ -23,5 +32,74 @@ namespace RPGTutorial
 {5}: {6}/{7}hp      {8}/{9}mp
 ********************************", hero.identifier, hero.currentHealth, hero.maxHealth, hero.currentMagic, hero.maxMagic, monster.identifier, monster.currentHealth, monster.maxHealth, monster.currentMagic, monster.maxMagic);
         }
+
+		string PrintChoice(){
+			string choice;
+			Console.WriteLine();
+			Console.Write(@"
+_____________________
+Please choose an action:
+(A)ttack:
+(D)efend:
+_____________________");
+            Console.WriteLine();
+            choice = Console.ReadLine();
+            return choice;
+        }
+
+		void ProcessChoice(string choice, Hero hero, Monster monster){
+			switch (choice){
+				case "A":
+				case "a":
+					Console.WriteLine();
+					Console.WriteLine("{0} attacks!", hero.identifier);
+					DealDamage(hero, monster);
+					monster.currentHealth -= damage;
+					Console.WriteLine("{0} hits the {1} for {2}hp of damage", hero.identifier, monster.identifier, damage);
+					break;
+				case "D":
+				case "d":
+					Console.WriteLine();
+					Console.WriteLine("{0} defends!", hero.identifier);
+					break;
+				default:
+					Console.WriteLine("I'm sorry, I didn't recognize that.");
+					Console.WriteLine();
+					choice = PrintChoice();
+					Console.WriteLine();
+					ProcessChoice(choice, hero, monster);
+					break;
+			}
+		}
+
+		int DealDamage(Hero hero, Monster monster){
+			int max;
+			int min;
+			rand = new Random();
+			max = hero.attackDamage - monster.defense;
+
+			if(max <= 0){
+				max = 1;
+			}
+
+			min = (int)(hero.attackDamage * .8) - monster.defense;
+
+			if(min <= 0){
+				min = 1;
+			}
+
+			damage = rand.Next(min, max);
+			return damage;
+		}
+
+		bool CheckHealth(int health){
+			bool alive;
+			if(health > 0){
+				alive = true;
+			} else {
+				alive = false;
+			}
+			return alive;
+		}
     }
 }
--- Tutorial1/Tutorial1/Monster.cs
+++ Tutorial1/Tutorial1/Monster.cs
@@ -3,6 +3,8 @@ namespace RPGTutorial
 {
     public class Monster
     {
+		Random rand;
+		protected int aiAttack, aiDefend, aiSpell;
 		public int currentHealth, maxHealth, currentMagic;
 		public int maxMagic, strength, defense, agility;
 		public int experience, gold, attackDamage;
@@ -10,6 +12,10 @@ namespace RPGTutorial
 		public bool isAlive;
 
         public Monster(){
+			aiAttack = 90;
+			aiDefend = 100;
+			aiSpell = 0;
+
 			currentHealth = 8;
 			maxHealth = 8;
 			currentMagic = 0;
@@ -23,5 +29,22 @@ namespace RPGTutorial
 			isAlive = true;
 			attackDamage = strength;
         }
+
+		public string AI(){
+			string choice;
+			int aiNumberChoice;
+			rand = new Random();
+			aiNumberChoice = rand.Next(1, 100);
+			if(aiNumberChoice < aiAttack){
+				choice = "Attack";
+			} else if (aiNumberChoice <= aiDefend && aiNumberChoice >= aiAttack){
+				choice = "Defend";
+			} else if (aiNumberChoice < aiSpell && aiNumberChoice > aiDefend){
+				choice = "Spell";
+			} else{
+				choice = "Flee";
+			}
+			return choice;
+		}
     }
 }
